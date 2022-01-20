bc776578d6ebaf97361c370c0fb90906bc7a5dc7
Zisho Astaru <66167985+ZishoAstaruID@users.noreply.github.com>
2022-01-20 22:35:08 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "IkuAPI",
+  "version": "1.0.0",
+  "description": "Official Package For Aeryuma Development (Modified)",
+  "main": "index.js",
+  "keywords": [
+    "nekos",
+    "nekos.life"
+  ],
+  "author": "Kanaka Nakazawa",
+  "license": "MIT"
+}
